7ad076556ed31606af6b0b9af972f4a797c24660
Veirt <veirt@duck.com>
2025-07-30 22:24:47 +0800

    fix: not found

--- serve.ts
+++ serve.ts
@@ -1,3 +1,4 @@
+import notFound from "lume/middlewares/not_found.ts";
 import Server from "lume/core/server.ts";
 import cacheBusting from "lume/middlewares/cache_busting.ts";
 import expires from "lume/middlewares/expires.ts";
@@ -6,5 +7,10 @@ const server = new Server();
 
 server.use(cacheBusting());
 server.use(expires());
+server.use(
+  notFound({
+    page404: "/404.html",
+  }),
+);
 
 server.start();
